e419b95e306ea4528254558139509e5fabf7a369
LavrL <llembersk@gmail.com>
2020-02-06 13:40:05 +0200

    added _redirects file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
@@ -35,6 +36,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html"
-        })
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/utils/_redirects'),
+                to: path.resolve(__dirname, 'build/')
+            }
+        ]),
     ]
 };
