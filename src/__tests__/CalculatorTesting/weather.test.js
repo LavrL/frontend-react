643dbe108c2335ec05579b83295bea2a36c51a04
LavrL <llembersk@gmail.com>
2020-04-18 19:14:10 +0300

    added more tests for Weather component

--- src/__tests__/CalculatorTesting/weather.test.js
+++ src/__tests__/CalculatorTesting/weather.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Weather from '../../components/Weather/Weather';
+import renderer from 'react-test-renderer';
 import { mount, configure } from 'enzyme';
 import axios from 'axios';
 import { REACT_APP_WEATHER_APPID } from '../../utils/constants';
+import { fetchData } from '../../utils/fetchData';
+
 jest.mock('axios');
 
 configure({ adapter: new Adapter() });
@@ -11,9 +14,12 @@ configure({ adapter: new Adapter() });
 describe('Calculator testing', () => {
     let wrapper;
 
-
-    beforeEach(() => {
+    beforeAll(() => {
         wrapper = mount(<Weather />);
+    });
+
+    afterAll(() => {
+        wrapper.unmount();
     });
 
     test('should return weather component initial state', () => {
@@ -31,25 +37,40 @@ describe('Calculator testing', () => {
         })
     });
 
+    test('Weather component renders correctly', () => {
+        const tree = renderer
+            .create(<Weather />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('render 2 input fields and 1 button GET WEATHER', () => {
+        expect(wrapper.find('input').length).toBe(2);
+        expect(wrapper.find('button').length).toBe(1);
+
+    })
+
     test('fetches successfully data from OpenWeather API', async () => {
-        const API = 'https://api.openweathermap.org/data/2.5/weather';
-
-        const fetchData = async (city, country, REACT_APP_WEATHER_APPID) => {
-            const url = `${API}?q=${city},${country}&units=metric&appid=${REACT_APP_WEATHER_APPID}`;
-
-            return await axios.get(url);
-        };
-
         const data = {
-                result: {
-                    temp: '10',
-                    minTemp: '6',
-                    maxTemp: '12',
-                    description: 'rain'
-                }
+            result: {
+                temp: '10',
+                minTemp: '6',
+                maxTemp: '12',
+                description: 'rain'
+            }
         };
-        
+
         axios.get.mockImplementationOnce(() => Promise.resolve(data));
         await expect(fetchData('Riga', 'Latvia', REACT_APP_WEATHER_APPID)).resolves.toEqual(data);
+
+        expect(axios.get).toHaveBeenCalledTimes(1);
+
+    });
+
+    test('fetches erroneously data from OpenWeather API', async () => {
+        const errorMsg = 'Network error';
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMsg)));
+
+        await expect(fetchData('Riga', 'Latvia', REACT_APP_WEATHER_APPID)).rejects.toThrow(errorMsg);
     });
 });
---
+++ src/utils/fetchData.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+export const API = 'https://api.openweathermap.org/data/2.5/weather';
+
+export const fetchData = async (city, country, REACT_APP_WEATHER_APPID) => {
+    const url = `${API}?q=${city},${country}&units=metric&appid=${REACT_APP_WEATHER_APPID}`;
+
+    return await axios.get(url);
+};
